00d0d6b53898cce43d3814fcd4b09685e244c934
AllwenWaker <AllwenWaker81@gmail.com>
2016-07-03 08:43:32 -0700

    Intelligent Bot!

---
+++ plugins/Allwen.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+   send_document(get_receiver(msg), "./data/allwen.webp", ok_cb, false)
+end
+
+return {
+patterns = {
+"^[Aa][Ll}[Ll][Ww][Ee][Nn]$",
+"^[Aa][Ll][Ee][Nn]$",
+"^الن$",
+"^آلن$"
+},
+run = run
+}
+
+end
